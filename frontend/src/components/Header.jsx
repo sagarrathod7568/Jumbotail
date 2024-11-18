d9f7a348659878784992f6cce1906f70372ebd12
sagarrathod7568 <sagarrathod050@gmail.com>
2024-11-18 17:06:49 +0530

    nav-fixed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,11 +2,13 @@
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold">E-Commerce Shipping Estimator</h1>
-      <p className="text-sm mt-1">
-        Easily calculate shipping charges and find the nearest warehouse!
-      </p>
+    <header className="relative mb-10">
+      <div className="bg-gray-800 text-white p-4 fixed top-0 left-0 right-0">
+        <h1 className="text-2xl font-bold">E-Commerce Shipping Estimator</h1>
+        <p className="text-sm mt-1">
+          Easily calculate shipping charges and find the nearest warehouse!
+        </p>
+      </div>
     </header>
   );
 };
